cdcca2b56d6db1431652826d2e0e8c62901d74de
DenimCity <altidorj1@gmail.com>
2019-12-09 08:10:32 -0500

    inital setup

---
+++ App/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using GraphQL;
+using GraphQL.Types;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var shema = Schema.For(@"
+            type Query {
+                hello: String
+            }");
+
+            var root = new { Hello = "Hello world"};
+            var json = shema.Execute(_ => {
+                _.Query = "{hello}";
+                _.Root = root;
+            });
+
+
+            Console.WriteLine(json);
+        }
+    }
+}
